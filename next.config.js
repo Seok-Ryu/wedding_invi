33f1eff94eb1e84e9d9618b34c847b68634c7444
Seok Ryu <gaorida@gmail.com>
2021-07-13 00:07:50 +0900

    modify config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   reactStrictMode: true,
+  exportPathMap: () => ({ // Next Export 에서 사용하는 값
+    "/": { page: "/" },
+    "/about": { page: "/about" },
+    "/sample": { page: "/sample" },
+  }),
+  images: {
+    loader: 'imgix',
+    domains: ['ryuliah.vercel.app', 'ryuliah.netlify.app'],
+  },
 }
